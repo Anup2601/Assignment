df3af508a8fc495f95bde73a4ba37b33824cb3f2
Anup Mishra <anupm0873@gmail.com>
2025-08-14 02:56:47 +0530

    fix error

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,12 +14,24 @@ dotenv.config({ path: "../.env" });
 // Express app
 const app = express();
 const server = http.createServer(app);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://assignment-sigma-pearl.vercel.app"
+];
 // Middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+    // Allow no-origin requests (like Postman or mobile apps)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
