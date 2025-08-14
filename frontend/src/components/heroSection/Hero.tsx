b94cf86c6dbe2e7e05e9a1b221b20086d2e8f680
Anup Mishra <anupm0873@gmail.com>
2025-08-14 05:54:18 +0530

    Update Hero.tsx

--- frontend/src/components/heroSection/Hero.tsx
+++ frontend/src/components/heroSection/Hero.tsx
@@ -4,7 +4,7 @@ import { HeroImage } from "./HeroImage";
 
 function Hero() {
   return (
-    <main className="overflow-hidden relative m-5 mx-auto max-w-none bg-blue-600 rounded-3xl h-[524px] max-md:p-5 max-md:h-auto max-md:max-w-[991px] max-md:min-h-[524px] max-sm:p-4 max-sm:max-w-screen-sm max-sm:rounded-2xl">
+    <main className="overflow-hidden relative m-5  max-w-none bg-blue-600 rounded-3xl h-[524px] max-md:p-5 max-md:h-auto max-md:max-w-[991px] max-md:min-h-[524px] max-sm:p-4 max-sm:max-w-screen-sm max-sm:rounded-2xl">
       <div className="flex absolute flex-col gap-9 items-start h-[347px] left-[42px] top-[89px] w-[764px] max-md:static max-md:gap-6 max-md:mx-auto max-md:my-0 max-md:w-full max-md:h-auto max-md:max-w-[764px] max-sm:gap-5">
         <header>
           <h1 className="text-4xl font-bold text-slate-50 w-[744px] max-md:w-full max-md:text-3xl max-sm:text-3xl max-sm:leading-tight">
